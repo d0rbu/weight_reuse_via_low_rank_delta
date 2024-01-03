5199245da70fd2c38b4aaa730efcc59fa5c47afc
d0rb <hacperu2010@gmail.com>
2024-01-03 12:24:48 -0600

    heatmaps for main viz

--- visualize/main.py
+++ visualize/main.py
@@ -1,24 +1,86 @@
 import os
 import json
+import math
 import matplotlib.pyplot as plt
-from typing import Mapping, Sequence
+import numpy as np
+from typing import Mapping, Tuple
 
+
+ACCURACY_NAMES = ["acc", "acc,none"]
+
+def get_avg_accuracy(task_results: Mapping) -> float:
+    num_accs = 0
+    sum_accs = 0
+    for task, results in task_results.items():
+        for acc_name in ACCURACY_NAMES:
+            if acc_name in results:
+                num_accs += 1
+                sum_accs += results[acc_name]
+                break
+
+    return sum_accs / num_accs
 
 def single_param_rank_layer_graph(full_results: Mapping) -> None:
+    param_heatmaps = {}
+    num_ranks = len(full_results)
+    num_layers = len(next(iter(full_results.values())))
+    ranks = sorted(full_results.keys())
+    ranks_idx = {rank: idx for idx, rank in enumerate(ranks)}
+
     for rank, rank_results in full_results.items():
-        for params, param_results in rank_results.items():
+        for param, param_results in rank_results.items():
+            param: str = param[0]
+            rank_idx: int = ranks_idx[rank]
+            if param in param_heatmaps:
+                heatmap = param_heatmaps[param]
+            else:
+                heatmap = np.empty((num_ranks, num_layers))
+                param_heatmaps[param] = heatmap
+
             for base_layer, task_results in param_results.items():
                 base_layer: int = base_layer[0]
-                for task, results in task_results.items():
-                    pass
+                avg_accuracy = get_avg_accuracy(task_results)
+                heatmap[rank_idx, base_layer] = avg_accuracy
+
+    for param, heatmap in param_heatmaps.items():
+        plt.imshow(heatmap)
+        plt.title(f"{param} Avg Acc")
+        plt.xlabel("Base layer")
+        plt.ylabel("Rank")
+        plt.xticks(range(num_layers))
+        plt.yticks(range(num_ranks), ranks)
+        plt.colorbar()
+        plt.show()
 
 def two_param_layer_layer_graph(full_results: Mapping) -> None:
+    rank_param_heatmaps = {
+        rank: {} for rank in full_results
+    }
+    num_layers = round(math.sqrt(len(next(iter(full_results.values())))))
+
     for rank, rank_results in full_results.items():
         for params, param_results in rank_results.items():
+            if params in rank_param_heatmaps[rank]:
+                heatmap = rank_param_heatmaps[rank][params]
+            else:
+                heatmap = np.empty((num_layers, num_layers))
+                rank_param_heatmaps[rank][params] = heatmap
+
             for base_layers, task_results in param_results.items():
-                first_base_layer, second_base_layer: int = base_layers
-                for task, results in task_results.items():
-                    pass
+                first_base_layer, second_base_layer = base_layers
+                avg_accuracy = get_avg_accuracy(task_results)
+                heatmap[first_base_layer, second_base_layer] = avg_accuracy
+
+    for rank, param_heatmaps in rank_param_heatmaps.items():
+        for params, heatmap in param_heatmaps.items():
+            plt.imshow(heatmap)
+            plt.title(f"Rank {rank} Avg Acc")
+            plt.xlabel(params[0])
+            plt.ylabel(params[1])
+            plt.xticks(range(num_layers))
+            plt.yticks(range(num_layers))
+            plt.colorbar()
+            plt.show()
 
 # Clean and split up full results based on number of parameters
 def clean_and_split_by_num_params(full_results: Mapping) -> Mapping[Mapping]:
